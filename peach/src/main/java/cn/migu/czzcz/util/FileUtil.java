c9df972c4f699035f2b274a0fc96cdaaca286fc7
Eli <czzcz@126.com>
2017-10-23 16:24:13 +0800

    初始化

---
+++ peach/src/main/java/cn/migu/czzcz/util/FileUtil.java
@@ -0,0 +1,18 @@
+/**
+ * All rights Reserved, Designed By MiGu
+ * Copyright:    Copyright(C) 2016-2020
+ * Company       MiGu  Co., Ltd.
+ */
+package cn.migu.czzcz.util;
+
+/**
+ * 描述
+ * 项目名称:  CzzczTest
+ * 包:        cn.migu.czzcz.util
+ * 类名称:    
+ * 类描述:    
+ * @author Chen Zhen
+ * @date:  2017/10/23
+ */
+public class FileUtil {
+}
